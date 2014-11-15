7fcdb9a06ace32352f94e2090443bbaafec78e62
Florent Flament <florent.flament-ext@cloudwatt.com>
2014-11-15 22:10:47 +0100

    Not using pbr - doesn't work on launchpad ppa.

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True,
+requires = open('requirements.txt').read().split()
+
+entry_points = {
+    'console_scripts' : ['ospurge = ospurge.ospurge:main']
+}
+
+README = open('README.md').read()
+
+setup(
+    name = 'ospurge',
+    version = '1.0.0',
+    description = 'OpenStack resources cleanup script',
+    long_description = README,
+    keywords = 'openstack',
+    author = 'Florent Flament',
+    author_email = 'florent.flament@cloudwatt.com',
+    url = 'https://github.com/stackforge/ospurge',
+    license = 'MIT',
+    packages = ['ospurge'],
+    install_requires = requires,
+    entry_points = entry_points
 )
